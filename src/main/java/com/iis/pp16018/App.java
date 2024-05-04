382e4e2915527c681e2b9ec26a1135cfb496297d
Farid Paredes <151954404+oFaridParedes@users.noreply.github.com>
2024-05-04 13:07:44 -0600

    primer commit

---
+++ src/main/java/com/iis/pp16018/App.java
@@ -0,0 +1,18 @@
+package com.iis.pp16018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
---
+++ src/test/java/com/iis/pp16018/AppTest.java
@@ -0,0 +1,20 @@
+package com.iis.pp16018;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
